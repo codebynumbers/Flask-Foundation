0c1dcc5aa509e4b9767d0c42be878efda8378eda
Rob Harrigan <harrigan.rob@gmail.com>
2015-04-18 19:39:57 -0400

    Fix the sqlite uri

--- appname/config_default.py
+++ appname/config_default.py
@@ -10,5 +10,5 @@ CACHE_NO_NULL_WARNING = True
 SQLALCHEMY_POOL_RECYCLE = 3600
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///tmp/appname.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/appname.db'
 #SQLALCHEMY_DATABASE_URI = 'mysql://appname:appname@localhost:3306/appname?charset=utf8'
